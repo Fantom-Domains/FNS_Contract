d924c55ad9e9a126b8daf923bb50258608edfa62
Blasco <blascokoa1@gmail.com>
2023-02-19 18:40:34 +0100

    Improve deployment script

--- scripts/fns.js
+++ scripts/fns.js
@@ -24,123 +24,120 @@ async function main() {
   const BulkRenewal   = await ethers.getContractFactory("BulkRenewal")
   const NameWrapper = await ethers.getContractFactory("NameWrapper")
   const StaticMetadataService = await ethers.getContractFactory("StaticMetadataService")
-  
+
   const signers = await ethers.getSigners();
   const accounts = signers.map(s => s.address)
-	
-  //console.log('ftm_namehash',namehash.hash("ftm"))	
-  
-  
+
+  //console.log('ftm_namehash',namehash.hash("ftm"))
+
+
   //ENS - Core
   const ens = await ENSRegistry.deploy();
   await ens.deployed()
   console.log(`address FNSRegistry (tx:${ens.address})...`);
   await delay(3000);
-  
+
   const resolver = await OwnedResolver.deploy();
   await resolver.deployed()
   console.log(`address OwnedResolver (tx:${resolver.address})...`);
   await delay(3000);
-  
+
   //Permanent Registrar
   const baseregistrar = await BaseRegistrar.deploy(ens.address,namehash.hash("ftm"));
   await baseregistrar.deployed()
   console.log(`address BaseRegistrar (tx:${baseregistrar.address})...`);
   await delay(3000);
-  
+
   const setSubnode = await ens.setSubnodeRecord(ZERO_HASH,labelhash("ftm"),baseregistrar.address,resolver.address,0);
   await setSubnode.wait(3);
-  
+
   //set Metadata
   const metadataHost = 'fns-metadata-service.appspot.com'
   const metadataUrl = `https://${metadataHost}/name/0x{id}`
-  
+
   const metadata = await StaticMetadataService.deploy(metadataUrl)
   await metadata.deployed()
   console.log("Metadata address:",metadata.address)
   await delay(3000);
-  
+
   const wrapper = await NameWrapper.deploy(ens.address, baseregistrar.address, metadata.address)
   await wrapper.deployed()
   console.log("Wrapper address:", wrapper.address)
   await delay(3000);
   //Public Resolver
-  
+
   const publicresolver = await PublicResolver.deploy(ens.address, wrapper.address); // ZERO_ADDRESS);
   await publicresolver.deployed()
   console.log(`address PublicResolver (tx:${publicresolver.address})...`);
   await delay(3000);
-  
+
   //Controller
   const priceoracle = await PriceOracle.deploy("0xf4766552d15ae4d256ad41b6cf2933482b0680dc",["100000000000000","10000000000000","1000000000000","100000000000"]);
   await priceoracle.deployed()
   console.log(`address oracle (tx:${priceoracle.address})...`);
   await delay(3000);
-  
-  
+
+
   const ethregistrarcontroller = await ETHRegistrarController.deploy(baseregistrar.address, priceoracle.address, 60,604800);
   await ethregistrarcontroller.deployed()
   console.log(`address FTMRegistrarController (tx:${ethregistrarcontroller.address})...`);
   await delay(3000);
-  
+
   await baseregistrar.addController(ethregistrarcontroller.address);
   console.log(`address BaseRegistrar (set controller..`);
   await delay(3000);
-  
+
   const bulkrenewal = await BulkRenewal.deploy(ens.address);
   await bulkrenewal.deployed()
   console.log(`address BulkRenewal (tx:${bulkrenewal.address})...`);
   await delay(3000);
-  
+
   const hashregisgrar = await HashRegistrar.deploy(ens.address, namehash.hash("ftm"), 0 );
   await hashregisgrar.deployed()
   console.log(`address HashRegistrar (tx:${hashregisgrar.address})...`);
   await delay(3000);
-  
+
   const setInterface_1 = await resolver.setInterface(namehash.hash("ftm"), '0x6ccb2df4', baseregistrar.address);
   await setInterface_1.wait(3);
   console.log(`Fixing interface_1...`);
-  
+
   const setInterface_2 = await resolver.setInterface(namehash.hash("ftm"), '0x018fac06', ethregistrarcontroller.address);
   await setInterface_2.wait(3);
   console.log(`Fixing interface_2...`);
-  
+
   const setInterface_3 = await resolver.setInterface(namehash.hash("ftm"), '0x3150bfba', bulkrenewal.address);
   await setInterface_3.wait(3);
   console.log(`Fixing interface_3...`);
-  
+
   const setInterface_4 = await resolver.setInterface(namehash.hash("ftm"), '0x7ba18ba1', hashregisgrar.address);
   await setInterface_4.wait(3);
   console.log(`Fixing interface_4...`);
-  
-  const setupResolver = await setupPublicResolver(ens, publicresolver, accounts);
-  await setupResolver.wait(3);
-  
+
+  await setupPublicResolver(ens, publicresolver, accounts);
+
   const fifsregistrar = await  FIFSRegistrar.deploy(ens.address, namehash.hash("test"));
   await fifsregistrar.deployed()
   console.log(`address FIFSRegistrar(TEST) (tx:${fifsregistrar.address})...`);
   await delay(3000);
-  
-  const setupReg = await setupRegistrar(ens, fifsregistrar);
-  await setupReg.wait(3);
-  
+
+  await setupRegistrar(ens, fifsregistrar);
+
   //const reverseRegistrar = await ReverseRegistrar.deploy(ens.address, resolver.address);
   const reverseRegistrar = await ReverseRegistrar.deploy(ens.address, publicresolver.address);
   await reverseRegistrar.deployed()
   console.log(`address ReverseRegistrar (tx:${reverseRegistrar.address})...`);
   await delay(3000);
-  
-  const setupRevReg = await setupReverseRegistrar(ens, reverseRegistrar, accounts);
-  await setupRevReg.wait(3);
+
+  await setupReverseRegistrar(ens, reverseRegistrar, accounts);
   console.log("Setup reverse Registrar Done");
-  
+
   //Set Approve for metadata
   await (await baseregistrar.setApprovalForAll(wrapper.address, true)).wait(3)
   await (await ens.setApprovalForAll(wrapper.address, true)).wait(3)
   console.log("Set Approve for metadata");
-  
+
   //dns-set
-  
+
   const RSASHA1Algorithm = await ethers.getContractFactory("RSASHA1Algorithm")
   const RSASHA256Algorithm = await ethers.getContractFactory("RSASHA256Algorithm")
   const P256SHA256Algorithm = await ethers.getContractFactory("P256SHA256Algorithm")
@@ -150,37 +147,37 @@ async function main() {
   const DNSSECImpl = await ethers.getContractFactory("DNSSECImpl")
   const TLDPublicSuffixList = await ethers.getContractFactory("TLDPublicSuffixList")
   const DNSRegistrar = await ethers.getContractFactory("DNSRegistrar")
-  
+
   const rsasha1algorithm = await RSASHA1Algorithm.deploy();
   await rsasha1algorithm.deployed()
   console.log(`address RSASHA1Algorithm (tx:${rsasha1algorithm.address})...`);
   await delay(3000);
-  
+
   const rsasha256algorithm = await RSASHA256Algorithm.deploy();
   await rsasha256algorithm.deployed()
   console.log(`address RSASHA256Algorithm (tx:${rsasha256algorithm.address})...`);
   await delay(3000);
-  
+
   const p256sha256algorithm = await P256SHA256Algorithm.deploy();
   await p256sha256algorithm.deployed()
   console.log(`address P256SHA256Algorithm (tx:${p256sha256algorithm.address})...`);
   await delay(3000);
-  
+
   const sha1digest = await SHA1Digest.deploy();
   await sha1digest.deployed()
   console.log(`address SHA1Digest (tx:${sha1digest.address})...`);
   await delay(3000);
-  
+
   const sha256digest = await SHA256Digest.deploy();
   await sha256digest.deployed()
   console.log(`address SHA256Digest (tx:${sha256digest.address})...`);
   await delay(3000);
-  
+
   const sha1nsec3digest = await SHA1NSEC3Digest.deploy();
   await sha1nsec3digest.deployed()
   console.log(`address SHA1NSEC3Digest (tx:${sha1nsec3digest.address})...`);
   await delay(3000);
-  
+
   const anchors = realAnchors.slice();
   const algorithms = {
         5: rsasha1algorithm,
@@ -195,12 +192,12 @@ async function main() {
   const nsec_digests = {
         1: sha1nsec3digest,
     };
-	
+
   const dnssecimpl = await DNSSECImpl.deploy(encodeAnchors(anchors));
   await dnssecimpl.deployed();
   console.log(`address DNSSECImpl (tx:${dnssecimpl.address})...`);
   await delay(3000);
-  
+
   const transactions = [];
   for(const [id, alg] of Object.entries(algorithms)) {
       const address = alg.address;
@@ -224,27 +221,27 @@ async function main() {
     }
 
   console.log(`Waiting on ${transactions.length} transactions setting DNSSEC parameters`);
-  await Promise.all(transactions.map((tx) => tx.wait(3)));   
+  await Promise.all(transactions.map((tx) => tx.wait(3)));
   console.log(`Done on ${transactions.length} transactions setting DNSSEC parameters`);
-  
+
   const tldpublicsuffixlist = await TLDPublicSuffixList.deploy();
   await tldpublicsuffixlist.deployed()
   console.log(`address TLDPublicSuffixList (tx:${tldpublicsuffixlist.address})...`);
   await delay(3000);
-  
-  
+
+
   const dnsregistrar = await DNSRegistrar.deploy(dnssecimpl.address, tldpublicsuffixlist.address, ens.address);
   await dnsregistrar.deployed()
   console.log(`address DNSRegistrar (tx:${dnsregistrar.address})...`);
   await delay(3000);
-	
+
   //tld-set
-	
+
   const registry = await ethers.getContractAt('FNSRegistry',ens.address);
   await registry.setSubnodeOwner(ZERO_HASH, labelhash("futbol"), dnsregistrar.address);
   console.log('Set Tld',registry.address)
   await delay(3000);
-  
+
   //test
   // const transactions_commit = [];
   // const transactions_register = [];
@@ -255,13 +252,13 @@ async function main() {
 	  // const commitment = await ethregistrarcontroller.makeCommitmentWithConfig(tx.fns,tx.account,web3StringToBytes32(''),resolverAddr,tx.account);
 	  // await ethregistrarcontroller.commit(commitment)
 	  // console.log('commitment',commitment)
-	  
+
   // }))
-  
-  
-  
+
+
+
   //await delay(60000)
-  
+
   // await Promise.all(jsonData.map(async (tx)=>{
 	  // const commitment = await ethregistrarcontroller.makeCommitmentWithConfig(tx.fns,tx.account,web3StringToBytes32(''),resolverAddr,tx.account);
 	  // const timestamp = await ethregistrarcontroller.commitments(commitment)
@@ -274,9 +271,9 @@ async function main() {
 		  // await ethregistrarcontroller.registerWithConfig(tx.fns,tx.account,web3StringToBytes32(secret),resolverAddr,tx.account,{ value: bufferprice, gasLimit });
 		  // console.log(`Registered for (account:${tx.account}, fns:${tx.fns}.ftm)...`);
 	  // }
-	  
+
   // }))
-  
+
 };
 
 function web3StringToBytes32(text) {
@@ -288,19 +285,27 @@ function web3StringToBytes32(text) {
 
 async function setupPublicResolver(ens, resolver, accounts) {
 
-  await ens.setSubnodeOwner(ZERO_HASH, labelhash("resolver"), accounts[0]);
+  const subNode = await ens.setSubnodeOwner(ZERO_HASH, labelhash("resolver"), accounts[0]);
+  await subNode.wait(3);
   //await ens.setSubnodeRecord(namehash.hash("ftm"), labelhash("resolver"), accounts[0], resolver.address, 0 );
-  await ens.setResolver(namehash.hash("resolver"), resolver.address);
-  await resolver['setAddr(bytes32,address)'](namehash.hash("resolver"), resolver.address);
+  const setresolver = await ens.setResolver(namehash.hash("resolver"), resolver.address);
+  await setresolver.wait(3);
+
+  const setadd = await resolver['setAddr(bytes32,address)'](namehash.hash("resolver"), resolver.address);
+  await setadd.wait(3);
 }
 
 async function setupRegistrar(ens, registrar) {
-  await ens.setSubnodeOwner(ZERO_HASH, labelhash("test"), registrar.address);
+  const setSubNode = await ens.setSubnodeOwner(ZERO_HASH, labelhash("test"), registrar.address);
+  await setSubNode.wait(3);
 }
 
 async function setupReverseRegistrar(ens, reverseRegistrar, accounts) {
-  await ens.setSubnodeOwner(ZERO_HASH, labelhash("reverse"), accounts[0]);
-  await ens.setSubnodeOwner(namehash.hash("reverse"), labelhash("addr"), reverseRegistrar.address);
+  const setSubNode = await ens.setSubnodeOwner(ZERO_HASH, labelhash("reverse"), accounts[0]);
+  await setSubNode.wait(3);
+
+  const setOwner = await ens.setSubnodeOwner(namehash.hash("reverse"), labelhash("addr"), reverseRegistrar.address);
+  await setOwner.wait(3);
 }
 
 const realAnchors = [
@@ -358,7 +363,7 @@ function encodeAnchors(anchors) {
       })
       .join('')
   );
-}; 
+};
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
